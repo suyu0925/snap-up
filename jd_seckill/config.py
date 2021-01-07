0e32a407e1a72fe66ff2723ddd5c015f0e4690de
suyu <13936562@qq.com>
2021-01-07 14:47:17 +0800

    不上传本地使用的config.ini

--- jd_seckill/config.py
+++ jd_seckill/config.py
@@ -1,12 +1,21 @@
 import os
 import configparser
+from jd_logger import logger
 
 
 class Config(object):
     def __init__(self, config_file='config.ini'):
         self._path = os.path.join(os.getcwd(), config_file)
         if not os.path.exists(self._path):
-            raise FileNotFoundError("No such file: config.ini")
+            if config_file == 'config.ini':
+                # 如果config.ini不存在，尝试读取config-template.ini
+                self._path = os.path.join(os.getcwd(), 'config-template.ini')
+                if not os.path.exists(self._path):
+                    raise FileNotFoundError("No such file: config.ini")
+                else:
+                    logger.info('use config-template.ini instead of config.ini')
+            else:
+                raise FileNotFoundError("No such file: config.ini")
         self._config = configparser.ConfigParser()
         self._config.read(self._path, encoding='utf-8-sig')
         self._configRaw = configparser.RawConfigParser()
